193ad7ffd0be38b07c8177e1342757e8ffae0cc6
drnefario21 <drnefario21@protonmail.com>
2022-07-25 13:14:04 +0200

    Changed the welcoming message to lang vX.X.X

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import functions
+import json
 
 try:
 	from termcolor import colored
@@ -13,30 +14,47 @@ except:
 	functions.clear()
 	from termcolor import colored
 
+
+
+data = []
+version = "0.0.0"
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+    version = data["version"]
+
+
 def main():
     while True:
         functions.clear()
 
-        print("Welcome!")
+        print(colored(f" --- lang v{version} ---", "green"))
         print("1. Learn words")
         print("2. View words")
         print("3. Add words (WIP)")
         print("4. Settings")
+        print("Type exit to quit.")
 
         usrinput = input(colored(" ==> ", "cyan"))
 
         if usrinput == "1":
             functions.learn_words()
+
         elif usrinput == "2":
             functions.view_words()
+
         elif usrinput == "3":
             functions.add_words()
+
         elif usrinput == "4":
             functions.settings_change()
+
         elif usrinput == "exit" or usrinput == "quit":
             exit()
+
         elif usrinput == "clear":
             pass
+
         else:
             print(colored("error", "red") + f": {usrinput}: invalid option.")
 
