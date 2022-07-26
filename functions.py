99a685733a3c2551d948b72e4dd436141a348550
Bob <drnefario21@protonmail.com>
2022-07-26 10:02:45 +0200

    Bug fixes, improved code readability, added comments

--- functions.py
+++ functions.py
@@ -5,15 +5,17 @@ import os
 import csv
 from termcolor import colored
 
-
 # const
-articles_list= "das", "die", "der", "el", "la", "lo", "los", "las"
+ARTICLES_LIST= "das", "die", "der", "el", "la", "lo", "los", "las"
+# The clear() function uses this variable. If you
+# are using Microsoft Windows, you should either
+# change it to "cls" or run the script inside a
+# Powershell.
+CLEAR_FUNCTION = "clear"
 
+# language
 lang = "es"
 lang_name = "Spanish"
-
-
-CLEAR_FUNCTION = "clear"
 
 
 def clear():
@@ -23,10 +25,10 @@ def clear():
 def settings(tp="load", set_dict=[]):
     if tp == "save":
         with open("settings.json", "w") as f:
-            json.dump(set_dict, f, indent=4)
+            json.dump(set_dict, f, indent=4) # wrting to json with indentation 4
     elif tp == "load":
         with open("settings.json", "r") as f:
-            return json.load(f)
+            return json.load(f) # returning the loaded settings file
 
 
 def load_words(lang=lang, name="all"):
@@ -49,6 +51,9 @@ def learn_words():
     name = ""
     settings_file = settings("load")
 
+    # If there is already a default language set in settings,
+    # change the lang_name to the language's name, i.e. "es"
+    # will be changed to Spanish.
     if settings_file["def_lang"] == "tr":
         lang = "tr"
         lang_name = "Turkish"
@@ -99,7 +104,7 @@ def learn_words():
         if q == 1:
             return
 
-    if lang in ["tr", "de", "es"]:
+    if lang in ["tr", "de", "es"]: # Technically this should always be executed
         print("Choose a category: ")
         print("1. All words")
         print("2. Colours")
@@ -109,52 +114,36 @@ def learn_words():
         print("6. Numbers")
         print("7. Numbers (extended)")
 
+        # This asks for user's input and based on what they enter
+        # the "name" variable will be changed. It is used for
+        # opennig .csv files with all the words.
         while True:
             usrinput = input(colored(" ==> ", "cyan")).lower()
 
             if usrinput == "1":
                 name = "all"
                 break
-
             elif usrinput == "2":
                 name = "colours"
                 break
-
             elif usrinput == "3":
                 name = "days"
                 break
-
             elif usrinput == "4":
                 name = "food"
                 break
-
             elif usrinput == "5":
                 name = "months"
                 break
-
             elif usrinput == "6":
                 name = "numbers"
                 break
-
             elif usrinput == "7":
                 name = "numbers_full"
                 break
-
-            elif usrinput == "clear":
-                clear()
-                print("Choose a category: ")
-                print("1. All words")
-                print("2. Colours")
-                print("3. Days")
-                print("4. Food")
-                print("5. Months")
-                print("6. Numbers")
-                print("7. Numbers (extended)")
-
             elif usrinput == "exit":
                 q = 1
                 break
-
             else:
                 print(colored("error", "red") + f": {usrinput}: invalid option.")
 
@@ -169,14 +158,16 @@ def learn_words():
     rows, fields = load_words(lang, name)
 
     for row in rows:
-        word = row[0]
-        lword = row[1]
+        word = row[0] # The English word
+        lword = row[1] # The word in another language the user selected
+        # Formality of the word. 0 = informal, 1 = formal, 2 = doesn't
+        # matter/can be used in both formal/informal situations
         formal = row[2]
-        article = row[3]
-        articles = settings_file["grammatical_articles"]
-        hint_number = 0
-        try_number = 0
-        formality = ""
+        article = row[3] # Article of the word, i.e.
+        articles = settings_file["grammatical_articles"] # Should the user enter grammatical articles? True/False
+        hint_number = 0 # How many hints the user has already used. When this reaches 3, the word skips
+        try_number = 0 # How many tries the user has made. Has a limit of 3
+        formality = "" # 0 = no, 1 = yes
 
         if formal == "0":
             formality = "no"
@@ -186,12 +177,12 @@ def learn_words():
             formality = "no"
 
         while True:
-            if formal != "2":
+            if formal != "2": # If formality doesn't matter
                 if articles == True:
                     print(f"Enter the {lang_name} word for " + colored(word, "magenta") + " with the proper grammatical article. Formal?: " + colored(formality, "cyan"))
                 else:
                     print(f"Enter the {lang_name} word for " + colored(word, "magenta") + ". Formal?: " + colored(formality, "cyan"))
-            else:
+            else: # If it does
                 if articles == True:
                     print(f"Enter the {lang_name} word for " + colored(word, "magenta") + " with the proper grammatical article.")
                 else:
@@ -201,42 +192,44 @@ def learn_words():
 
             usrinput = input().lower()
 
-            if article not in articles_list:
+            if article not in ARTICLES_LIST:
                 articles = False
 
-            if True:
+            if True: # idk why I added this
+                # If articles are enabled AND the user hasn't tried to use a hint/exit
                 if articles == True and usrinput != "hint" and usrinput != "exit":
+                    # If the word that the user is article+word, i.e. "el pan"
                     if usrinput == article + " " + lword.lower():
                         print(colored("Correct", "green") + "!")
                         break
                     else:
-                        try_number += 1
+                        try_number += 1 # Increment try_number by 1
 
-                        if try_number <= 2:
+                        if try_number <= 2: # If try_number hasn't reached 3
                             print(colored("Incorrect", "red") + ". Try again.")
-                        else:
+                        else: # Else, tell the user the word
                             print(colored("Incorrect", "red") + ". The right answer was: " + colored(article, "cyan"), colored(lword, "cyan"))
                             break
+                # If articles are disabled AND the user hasn't tried to use a hint/exit
                 elif articles != False and usrinput != "hint" and usrinput != "exit":
                     if usrinput == lword.lower():
                         print(colored("Correct", "green") + "!")
                         break
                     else:
-                        try_number += 1
+                        try_number += 1 # Increment try_number by 1
 
-                        if try_number <= 2:
+                        if try_number <= 2: # If try_number hasn't reached 3
                             print(colored("Incorrect", "red") + ". Try again.")
-                        else:
+                        else: # Else, tell the user the word
                             print(colored("Incorrect", "red") + ". The right answer was: " + colored(article, "cyan"), colored(lword, "cyan"))
                             break
                 elif usrinput == "hint":
                     hints = 3
-
+                    # I honestly don't even understand what this does
                     if len(lword) <= hints:
                         hints = len(lword) - 1
                     else:
                         pass
-
 
                     if hint_number < hints:
                         print(colored("Hint", "magenta") + f": {lword[hint_number]}")
@@ -246,6 +239,14 @@ def learn_words():
                 elif usrinput == "exit":
                     q = 1
                     break
+                elif usrinput == "":
+                    try_number += 1 # Increment try_number by 1
+
+                    if try_number <= 2: # If try_number hasn't reached 3
+                        print(colored("Incorrect", "red") + ". Try again.")
+                    else: # Else, tell the user the word
+                        print(colored("Incorrect", "red") + ". The right answer was: " + colored(article, "cyan"), colored(lword, "cyan"))
+                        break
                 else:
                     print("what")
             else:
@@ -277,12 +278,10 @@ def learn_words():
                         print(colored("Incorrect", "red") + ". The right answer was: " + colored(lword, "cyan"))
                         break
         
-        if q == 1:
+        if q == 1: # If the user typed "exit" then this will break the loop
             break
-        else:
-            pass
     
-    input("Press ENTER to go back...")
+    input("Press ENTER to go back...") # Self explanatory
 
 
 def view_words():
@@ -291,6 +290,7 @@ def view_words():
     q = 0
     settings_file = settings("load")
 
+    # The same chunk of code learn_words() has.
     if settings_file["def_lang"] == "tr":
         lang = "tr"
         lang_name = "Turkish"
@@ -337,7 +337,7 @@ def view_words():
             return
 
 
-    if lang in ["tr", "de", "es"]:
+    if lang in ["tr", "de", "es"]: # This should also be always executed
         print("Choose a category: ")
         print("1. All words")
         print("2. Colours")
@@ -462,18 +462,19 @@ def add_words():
         return
 
 
-    lword = ""
-    enword = ""
-    formality = 0
+    # This is probably useless. I am not deleting it because it can break the code
+    enword = "" # The English word
+    lword = "" # The word in the language the user has selected
+    formality = 0 # Formality. 0 = no, 1 = yes, 2 = doesn't matter. Set to 0 by default
 
     if lang in ["tr", "de", "es"]:
         print(colored("\x1B[3mi\x1B[0m", "green") + f": Enter an English name and then the {lang_name} counterpart afterwards.")
         print(colored("\x1B[3mi\x1B[0m", "green") + ": If you spelled a word incorrectly, you can type \"!back\"; it will cancel the process.")
 
         while True:
-            lword = ""
-            enword = ""
-            formality = 0
+            lword = "" # The English word
+            enword = "" # The word in the language the user has selected
+            formality = 0 # Formality. 0 = no, 1 = yes, 2 = doesn't matter. Set to 0 by default
 
             usrinput = input(colored(" en ==> "))
             if usrinput.lower() == "!back":
@@ -521,52 +522,44 @@ def settings_change():
 
 
     while True:
-        usrinput = input(colored(" ==> ", "cyan"))
+        usrinput = input(colored(" settings ==> ", "cyan"))
 
-        if usrinput == "1" or usrinput == "":
+        if usrinput == "1":
             print("Enter your preferred language")
-            usrinput = input("  [ES/de/tr] " + colored("==> ", "cyan")).lower()
+            usrinput = input(" [ES/de/tr] " + colored("==> ", "cyan")).lower()
 
             if usrinput == "tr" or usrinput == "turkish":
                 if usrinput == settings_file["def_lang"]:
                     print("Your language is already set to Turkish (tr).")
-
                     input("Press ENTER to go back...")
+
                 else:
                     settings_file["def_lang"] = "tr"
                     print("Successfully changed default language to Turkish (tr).")
                     settings("save", settings_file)
-
                     time.sleep(1.5)
-
                 break
 
             elif usrinput == "de" or usrinput == "german":
                 if usrinput == settings_file["def_lang"]:
                     print("Your language is already set to German (de).")
-
                     input("Press ENTER to go back...")
                 else:
                     settings_file["def_lang"] = "de"
                     print("Successfully changed default language to German (de).")
                     settings("save", settings_file)
-
                     time.sleep(1.5)
-
                 break
 
             elif usrinput == "es" or usrinput == "spanish":
                 if usrinput == settings_file["def_lang"]:
                     print("Your language is already set to Spanish (es).")
-
                     input("Press ENTER to go back...")
                 else:
                     settings_file["def_lang"] = "es"
                     print("Successfully changed default language to Spanish (es).")
                     settings("save", settings_file)
-
                     time.sleep(1.5)
-
                 break
 
             elif usrinput == "exit":
@@ -576,6 +569,8 @@ def settings_change():
                 print(colored("error", "red") + f": {usrinput}: ivalid language number/name/code.")
 
         elif usrinput == "2":
+            # This is set to 1 and if the user types y/yes/nothing it changes
+            # the formality setting to True.
             t = 0
 
             if settings_file["formality"] == False:
@@ -597,6 +592,8 @@ def settings_change():
                 pass
 
         elif usrinput == "3":
+            # This is set to 1 and if the user types y/yes/nothing it changes
+            # the formality setting to True.
             t = 0
 
             if settings_file["grammatical_articles"] == False:
@@ -619,11 +616,11 @@ def settings_change():
             print("Successfully changed to " + colored(settings_file["grammatical_articles"], "cyan") + ".")
 
         elif usrinput == "4":
-            print("Are you sure you want to reset all progress? This " + colored("cannot", "red") + " be undone.")
+            print("Are you sure you want to reset settings? This " + colored("cannot", "red") + " be undone.")
             confirm = input(" [y/N] " + colored("==> ", "magenta")).lower()
 
             if confirm == "y" or confirm == "yes":
-                settings_file["def_lang"] = "es"
+                settings_file["def_lang"] = ""
                 settings_file["formality"] = False
                 settings_file["grammatical_articles"] = True
 
